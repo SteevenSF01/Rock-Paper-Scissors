b412ad144a1567c3602aafac65aa515a8efbab34
SteevenSF01 <salgadosteeven@gmail.com>
2024-02-15 11:53:50 +0100

    fin button rules

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ function App() {
         <div className="w-[100%] h-[50%] flex justify-center items-end ">
         <Rock />
         </div>
-
+      </div>
+      <div className="flex justify-center mt-8">
+        <button className="border-2 border-[#606e85] text-white px-8 py-1 rounded-lg ">Rules</button>
       </div>
     </>
   );
