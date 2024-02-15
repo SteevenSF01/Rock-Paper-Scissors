68bd6ca75d991b9a01d3d68c7977dddccb04d15d
SteevenSF01 <salgadosteeven@gmail.com>
2024-02-15 22:44:00 +0100

    ajout dossier github

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Rock-Paper-Scissors/',
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-  }
+  base: '/Rock-Paper-Scissors/',
 })
