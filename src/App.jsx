584ea243792f1d244c27f1663a42984760878a83
SteevenSF01 <salgadosteeven@gmail.com>
2024-02-15 10:50:28 +0100

    container main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/header/Header'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
-
   return (
     <>
       <Header />
+      <div className="w-[100%] h-[50%] border-4 my-12 containerMain">
+        
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
